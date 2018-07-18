129c225a03f4b453e4c1f8257762ce2945d8acd3
jacob <jacob.chalegoua@gmail.com>
2018-07-18 14:30:28 +0200

    create available rooms table

--- full-stack-hotels-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
+++ full-stack-hotels-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
@@ -10,21 +10,60 @@ export class AppComponent implements OnInit {
 
   public submitted:boolean;
   roomsearch : FormGroup;
-
+  rooms : Room[];
     ngOnInit() {
         this.roomsearch = new FormGroup({
             checkin: new FormControl(''),
             checkout: new FormControl('')
         });
+        
+        this.rooms = ROOMS;
     }
 
     onSubmit({value,valid}: {value:Roomsearch, valid:boolean}) {
       console.log(value);
     }
-
+    
+    reserveRoom(value:string) {
+        console.log("room id for reservation: " + value);
+    }
 }
+
+
 
 export interface Roomsearch {
   checkin:string;
   checkout:string;
 }
+
+
+
+export interface Room {
+  id:string;
+  roomNumber:string;
+  price:string;
+  links:string;
+}
+
+
+var ROOMS:Room[] = [
+{
+  "id": "232312",
+  "roomNumber" : "409", 
+  "price" : "20", 
+  "links" : ""      
+}    ,
+{
+  "id": "3423425",
+  "roomNumber" : "410", 
+  "price" : "30", 
+  "links" : ""      
+}    ,
+
+{
+  "id": "34234",
+  "roomNumber" : "411", 
+  "price" : "55", 
+  "links" : ""      
+}   
+]
